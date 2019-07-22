aca20a677c526d9d177b08ecc7f368f6801acffa
xzw <1170316551@qq.com>
2019-07-22 21:34:50 +0800

    test添加git仓库

---
+++ src/cn/rng/test/Hi.java
@@ -0,0 +1,11 @@
+package cn.rng.test;
+
+public class Hi {
+    public static void main(String[] args) {
+        System.out.println("hihihi");
+        System.out.println("hihihi");
+        System.out.println("hihihi");
+        System.out.println("hihihi");
+        System.out.println("hihihi");
+    }
+}
